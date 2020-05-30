3afd46b5de4d281a30d330f3afcb7f96ceedf3de
Jonah Dlin <jdlin@uwaterloo.ca>
2020-05-30 14:13:35 -0400

    Updates

---
+++ precache-manifest.74596d6618056e2182c55b6b9de52cd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "16aa46739a59df4dc88e",
+    "url": "/evolution/static/css/main.0abdba1c.chunk.css"
+  },
+  {
+    "revision": "16aa46739a59df4dc88e",
+    "url": "/evolution/static/js/main.e1c413a3.chunk.js"
+  },
+  {
+    "revision": "0c798a68d947fb07aa54",
+    "url": "/evolution/static/js/runtime~main.d68a2c1a.js"
+  },
+  {
+    "revision": "af1fdf74688e0144936b",
+    "url": "/evolution/static/js/2.d7ea2c6c.chunk.js"
+  },
+  {
+    "revision": "b560815a84384120827e3e4ce22285e3",
+    "url": "/evolution/index.html"
+  }
+];
